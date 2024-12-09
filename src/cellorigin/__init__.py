c9f99d85b9d2c927ea226b35e0be05e8118108be
whistle-ch0i <155410914+whistle-ch0i@users.noreply.github.com>
2024-12-09 16:28:52 +0900

    Update __init__.py

--- src/cellorigin/__init__.py
+++ src/cellorigin/__init__.py
@@ -1,8 +1,8 @@
 # Import functions from RDim.py
-from .RDim import calculate_relative_dimension, run_single_source
+from .rdim import calculate_relative_dimension, run_single_source
 
 # Import functions from MIC.py
-from .MIC import generate_mi_mat
+from .mic import generate_mi_mat
 
 # Import functions from construct_graph.py
 from .construct_graph import generate_graph
